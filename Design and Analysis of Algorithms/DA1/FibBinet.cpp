23da2be591fb1156750826628bd6ab7c1dff31da
Vishruth Devan <vishruthdevan234@gmail.com>
2021-08-15 21:44:45 +0530

    Find nth fibonacci number using Binet's formula (divide and conquer algo)

---
+++ Design and Analysis of Algorithms/DA1/FibBinet.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+double phi = (sqrt(5) + 1)/2;
+
+
+double fib(int n){
+    if (n == 2)
+        return phi*phi;
+    else if (n%2 == 0)
+        return pow(fib(n/2), 2);
+    else
+        return pow(fib(n/2), 2)*phi;
+}
+
+int main(){
+    cout << round(fib(4)/sqrt(5));
+}
