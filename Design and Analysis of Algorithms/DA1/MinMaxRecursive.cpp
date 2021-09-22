9d661e8969be7c71f62bf0df11c9737b917ed079
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-22 16:45:35 +0530

    Chnaged function names

--- Design and Analysis of Algorithms/DA1/MinMaxRecursive.cpp
+++ Design and Analysis of Algorithms/DA1/MinMaxRecursive.cpp
@@ -3,7 +3,7 @@
 #include<limits>
 using namespace std;
 
-void minmax(vector<int> &arr, int &min, int &max, int low, int high){
+void MinMaxDaC(vector<int> &arr, int &min, int &max, int low, int high){
     if(low==high){
         min = arr[low];
         max = arr[low];
@@ -11,7 +11,7 @@ void minmax(vector<int> &arr, int &min, int &max, int low, int high){
     }
 
     if(low==high-1){
-        if(arr[low]<arr[high]){
+        if(arr[low]<=arr[high]){
             if(arr[low]<min)
                 min = arr[low];
             if(arr[high]>max)
@@ -32,18 +32,19 @@ void minmax(vector<int> &arr, int &min, int &max, int low, int high){
         int max1 = INT_MIN;
         int min2 = INT_MAX;
         int max2 = INT_MIN;
-        minmax(arr, min1, max1, low, mid);
-        minmax(arr, min2, max2, mid+1, high);
+        MinMaxDaC(arr, min1, max1, low, mid);
+        MinMaxDaC(arr, min2, max2, mid+1, high);
         min = (min1 < min2)? min1:min2;
         max = (max1 > max2)? max1:max2;
     }
 }
 
 int main(){
-    vector<int> arr{12, 23, 34, 45, 56, 67, 78, 89, 90};
-    int min = numeric_limits<int>::max();
-    int max = numeric_limits<int>::lowest();
-    minmax(arr, min, max, 0, 3);
-    cout << min << " " << max;
+    vector<int> arr{67, 78, 12, 23, 89, 90, 90, 89, 56, 178};
+    int min = INT_MAX;
+    int max = INT_MIN;
+    MinMaxDaC(arr, min, max, 0, 9);
+    cout << "Minimum: " << min << endl;
+    cout << "Maximum: " << max << endl;
     return 0;
 }
