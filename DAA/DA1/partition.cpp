8cf81d5f08da6c18c857023a2cf17ddbf4dcd934
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-23 20:26:26 +0530

    Add divide and conquer approach

--- DAA/DA1/partition.cpp
+++ DAA/DA1/partition.cpp
@@ -89,62 +89,62 @@ void partition(vector<vector<int>> set){
 
 
 
-// void merge(vector<vector<vector<int>>> &f, int left, int mid, int right){
-//     int len1 = mid - left + 1;
-//     int len2 = right - mid;
+void merge(vector<vector<vector<int>>> &f, int left, int mid, int right){
+    int len1 = mid - left + 1;
+    int len2 = right - mid;
 
-//     vector<vector<vector<int>>> leftSet(len1);
-//     vector<vector<vector<int>>> rightSet(len2);
+    vector<vector<vector<int>>> leftSet(len1);
+    vector<vector<vector<int>>> rightSet(len2);
 
-//     for (int i = 0; i < len1; i++)
-//         leftSet[i] = f[left + i];
-//     for (int j = 0; j < len2; j++)
-//         rightSet[j] = f[mid + 1 + j];
+    for (int i = 0; i < len1; i++)
+        leftSet[i] = f[left + i];
+    for (int j = 0; j < len2; j++)
+        rightSet[j] = f[mid + 1 + j];
 
-//     int i=0, j=0, k=left;
+    int i=0, j=0, k=left;
 
-//     while(i<len1 && j<len2){
-//         if(lesserthan(rightSet[j].front(), leftSet[i].front())){
-//             f[k] = leftSet[i];
-//             i++;
-//         }
-//         else if(greaterthan(rightSet[j].front(), leftSet[i].front())){
-//             f[k] = rightSet[j];
-//             j++;
-//         }
+    while(i<len1 && j<len2){
+        if(lesserthan(rightSet[j].front(), leftSet[i].front())){
+            f[k] = leftSet[i];
+            i++;
+        }
+        else if(greaterthan(rightSet[j].front(), leftSet[i].front())){
+            f[k] = rightSet[j];
+            j++;
+        }
 
-//         else{
-//             f[k] = leftSet[i];
-//             f[k].push_back(rightSet[j].front());
-//             f.erase(f.begin() + (mid + 1 + j));
-//             i++;
-//             j++;
-//         }
-//         k++;
-//     }
+        else{
+            f[k] = leftSet[i];
+            f[k].push_back(rightSet[j].front());
+            f.erase(f.begin() + (mid + 1 + j));
+            i++;
+            j++;
+        }
+        k++;
+    }
 
-//     while (i < len1) {
-//         f[k] = leftSet[i];
-//         i++;
-//         k++;
-//     }
+    while (i < len1) {
+        f[k] = leftSet[i];
+        i++;
+        k++;
+    }
 
-//     while (j < len2) {
-//         f[k] = rightSet[j];
-//         j++;
-//         k++;
-//     }
-// }
+    while (j < len2) {
+        f[k] = rightSet[j];
+        j++;
+        k++;
+    }
+}
 
 
-// void partition2(vector<vector<vector<int>>> &f, int left, int right){
-//     if (left>=right)
-//         return;
-//     int mid = (left+right)/2;
-//     partition2(f, left, mid);
-//     partition2(f, mid+1, right);
-//     merge(f, left, mid, right);
-// }
+void partition2(vector<vector<vector<int>>> &f, int left, int right){
+    if (left>=right)
+        return;
+    int mid = (left+right)/2;
+    partition2(f, left, mid);
+    partition2(f, mid+1, right);
+    merge(f, left, mid, right);
+}
 
 int main(){
     vector<vector<int>> set{vector<int>{67, 6, 4}, vector<int>{67, 5, 4}, vector<int>{23, 2, 4}, vector<int>{22, 4, 4}};
