218748b92714f4b15aaf58dcc9c7e1d69a41496b
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-11 23:37:21 +0530

    Add a plain text resource

---
+++ JAVA/REST API/duplicates/src/main/java/vishruth/javada2/duplicates/resources/DuplicatesResource.java
@@ -0,0 +1,17 @@
+package vishruth.javada2.duplicates.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class DuplicatesResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "foobar";
+	}
+}
+
