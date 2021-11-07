667d6fc9bc79d28212cfacbf2cfd91ed5fecde37
Vishruth <vishruthdevan@gmail.com>
2021-11-07 18:31:05 +0530

    Final commit

--- DAA/DA2/lcs.cpp
+++ DAA/DA2/lcs.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 #include<string>
+
 using namespace std;
 
 int max(int a, int b){
     return (a > b) ? a : b;
 }
-
 
 void print(vector<vector<int>> C, vector<vector<char>> direction, string s, int i, int j){
     if(i==0 || j==0)
@@ -22,7 +22,6 @@ void print(vector<vector<int>> C, vector<vector<char>> direction, string s, int 
         print(C, direction, s, i, j-1);
 }
 
-
 void lcs(string s1, string s2){
 
     int len1 = s1.length();
@@ -30,7 +29,7 @@ void lcs(string s1, string s2){
 
     vector<vector<int>> C(len1+1, vector<int>(len2+1));
     vector<vector<char>> direction(len1+1, vector<char>(len2+1));
-
+    // direction is useful when printing out the longest subsequence
     for(int i=0; i<=len1; i++){
         for(int j=0; j<=len2; j++){
             if(i==0 || j==0){
@@ -50,15 +49,18 @@ void lcs(string s1, string s2){
             }
         }
     }
-    cout << "Length of longest common substring: " << C[len1][len2] << endl;
+    cout << "\nLength of longest common subsequence: " << C[len1][len2] << endl;
+    
+    cout << "\nLongest common subsequence: ";
     print(C, direction, s1, len1, len2);
+    cout << "\n\n";
 }
 
 int main(){
     string s1, s2;
     cout << "Enter string 1:\n";
     getline(cin, s1);
-    cout << "Enter string 1:\n";
+    cout << "Enter string 2:\n";
     getline(cin, s2);
     lcs(s1, s2);
 }
