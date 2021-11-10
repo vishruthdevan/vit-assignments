2a3654011a0d4eb0eb4d1d8621d96eb1a0f5b413
Vishruth <vishruthdevan@gmail.com>
2021-11-10 20:57:10 +0530

    Final commit

--- DAA/DA4/ford_fulkerson.cpp
+++ DAA/DA4/ford_fulkerson.cpp
@@ -6,18 +6,9 @@
 #include <string.h>
 using namespace std;
 
-// Number of vertices in given graph
-#define V 6
-
-/* Returns true if there is a path from source 's' to sink
-  't' in residual graph. Also fills parent[] to store the
-  path */
-bool bfs(int rGraph[V][V], int s, int t, int parent[])
+bool bfs(vector<vector<int>> &RG, int s, int t, vector<int> &parent, int V)
 {
-    // Create a visited array and mark all vertices as not
-    // visited
-    bool visited[V];
-    memset(visited, 0, sizeof(visited));
+    vector<bool> visited(V, false);
 
     vector<int> queue;
     queue.push_back(s);
@@ -31,15 +22,12 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
 
         for (int v = 0; v < V; v++)
         {
-            if (visited[v] == false && rGraph[u][v] > 0)
+            if (!visited[v] && RG[u][v] > 0)
             {
-                if (v == t)
-                {
-                    parent[v] = u;
-                    return true;
-                }
-                queue.push_back(v);
                 parent[v] = u;
+                if (v == t)
+                    return true;
+                queue.push_back(v);
                 visited[v] = true;
             }
         }
@@ -47,72 +35,54 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
     return false;
 }
 
-// Returns the maximum flow from s to t in the given graph
-int fordFulkerson(int graph[V][V], int s, int t)
+int fordFulkerson(vector<vector<int>> &C, int s, int t, int V)
 {
     int u, v;
 
-    // Create a residual graph and fill the residual graph
-    // with given capacities in the original graph as
-    // residual capacities in residual graph
-    int rGraph[V]
-              [V]; // Residual graph where rGraph[i][j]
-                   // indicates residual capacity of edge
-                   // from i to j (if there is an edge. If
-                   // rGraph[i][j] is 0, then there is not)
+    vector<vector<int>> RG(V, vector<int>(V));
     for (u = 0; u < V; u++)
         for (v = 0; v < V; v++)
-            rGraph[u][v] = graph[u][v];
+            RG[u][v] = C[u][v];
 
-    int parent[V]; // This array is filled by BFS and to
-                   // store path
+    vector<int> parent(V);
+    int max_flow = 0;
 
-    int max_flow = 0; // There is no flow initially
-
-    // Augment the flow while tere is path from source to
-    // sink
-    while (bfs(rGraph, s, t, parent))
+    while (bfs(RG, s, t, parent, V))
     {
-        // Find minimum residual capacity of the edges along
-        // the path filled by BFS. Or we can say find the
-        // maximum flow through the path found.
         int path_flow = INT_MAX;
         for (v = t; v != s; v = parent[v])
         {
             u = parent[v];
-            path_flow = min(path_flow, rGraph[u][v]);
+            path_flow = path_flow < RG[u][v] ? path_flow : RG[u][v];
         }
 
-        // update residual capacities of the edges and
-        // reverse edges along the path
         for (v = t; v != s; v = parent[v])
         {
             u = parent[v];
-            rGraph[u][v] -= path_flow;
-            rGraph[v][u] += path_flow;
+            RG[u][v] -= path_flow;
+            RG[v][u] += path_flow;
         }
 
-        // Add path flow to overall flow
         max_flow += path_flow;
     }
 
-    // Return the overall flow
     return max_flow;
 }
 
-// Driver program to test above functions
 int main()
 {
-    // Let us create a graph shown in the above example
-    int graph[V][V] = {{0, 16, 13, 0, 0, 0},
-                       {0, 0, 10, 12, 0, 0},
-                       {0, 4, 0, 0, 14, 0},
-                       {0, 0, 9, 0, 0, 20},
-                       {0, 0, 0, 7, 0, 4},
-                       {0, 0, 0, 0, 0, 0}};
+    int V = 8;
+    vector<vector<int>> C = {{0, 13, 10, 10, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 24, 0, 0, 0},
+                             {0, 5, 0, 15, 0, 0, 7, 0},
+                             {0, 0, 0, 0, 0, 0, 15, 0},
+                             {0, 0, 0, 0, 0, 1, 0, 9},
+                             {0, 0, 0, 0, 0, 0, 6, 13},
+                             {0, 0, 0, 0, 0, 0, 0, 16},
+                             {0, 0, 0, 0, 0, 0, 0, 0}};
 
     cout << "The maximum possible flow is "
-         << fordFulkerson(graph, 0, 5);
+         << fordFulkerson(C, 0, 7, V);
 
     return 0;
 }
