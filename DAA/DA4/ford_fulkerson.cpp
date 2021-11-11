2e7e7551c46b5320d68799356eb083e430580d7e
Vishruth <vishruthdevan@gmail.com>
2021-11-11 19:32:26 +0530

    Final commit

--- DAA/DA4/ford_fulkerson.cpp
+++ DAA/DA4/ford_fulkerson.cpp
@@ -1,12 +1,11 @@
-// C++ program for implementation of Ford Fulkerson
-// algorithm
 #include <iostream>
 #include <limits.h>
 #include <vector>
-#include <string.h>
+
 using namespace std;
 
-bool bfs(vector<vector<int>> &RG, int s, int t, vector<int> &parent, int V)
+// Breadth first search to find any path from s to t in the residual graph RG
+bool bfs(vector<vector<int>> &RG, vector<int> &parent, int s, int t, int V)
 {
     vector<bool> visited(V, false);
 
@@ -17,72 +16,109 @@ bool bfs(vector<vector<int>> &RG, int s, int t, vector<int> &parent, int V)
 
     while (!queue.empty())
     {
-        int u = queue.front();
+        //Popping the front of the queue to start traversing
+        int i = queue.front();
         queue.erase(queue.begin());
 
-        for (int v = 0; v < V; v++)
+        for (int j = 0; j < V; j++)
         {
-            if (!visited[v] && RG[u][v] > 0)
+            if (!visited[j] && RG[i][j] > 0)
             {
-                parent[v] = u;
-                if (v == t)
+                parent[j] = i;
+                if (j == t)
+                    //If an augmenting path is found from s to t, we return 'true'
+                    // after which the flow is augmented
                     return true;
-                queue.push_back(v);
-                visited[v] = true;
+
+                //pushing all neighbours of the current vertex to traverse through later
+                queue.push_back(j);
+                visited[j] = true;
             }
         }
     }
+    // if no path is found from s to t then we return 'false'
     return false;
 }
 
 int fordFulkerson(vector<vector<int>> &C, int s, int t, int V)
 {
-    int u, v;
-
+    // RG is the residual graph containing the residual capacities
+    // that is initialized with the capacities C of each edge (i, j)
     vector<vector<int>> RG(V, vector<int>(V));
-    for (u = 0; u < V; u++)
-        for (v = 0; v < V; v++)
-            RG[u][v] = C[u][v];
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            RG[i][j] = C[i][j];
 
     vector<int> parent(V);
-    int max_flow = 0;
+    int maxFlow = 0, pathFlow;
 
-    while (bfs(RG, s, t, parent, V))
+    // while an augmenting path exists from s to t
+    while (bfs(RG, parent, s, t, V))
     {
-        int path_flow = INT_MAX;
-        for (v = t; v != s; v = parent[v])
+        // find the mininum capacity edge among all the edges in that path
+        pathFlow = INT_MAX;
+        for (int j = t; j != s; j = parent[j])
         {
-            u = parent[v];
-            path_flow = path_flow < RG[u][v] ? path_flow : RG[u][v];
+            int i = parent[j];
+            pathFlow = pathFlow < RG[i][j] ? pathFlow : RG[i][j];
         }
 
-        for (v = t; v != s; v = parent[v])
+        // Update the edges and the reverse edges of the path
+        // in the residual graph after finding the minimum capacity of the path (pathFlow)
+        for (int j = t; j != s; j = parent[j])
         {
-            u = parent[v];
-            RG[u][v] -= path_flow;
-            RG[v][u] += path_flow;
+            int i = parent[j];
+            RG[i][j] -= pathFlow;
+            RG[j][i] += pathFlow;
         }
 
-        max_flow += path_flow;
+        // pathFlow is added to maxFlow and the process is repeated
+        // until no augmenting path exists from s to t
+        maxFlow += pathFlow;
     }
 
-    return max_flow;
+    return maxFlow;
 }
 
 int main()
 {
-    int V = 8;
-    vector<vector<int>> C = {{0, 13, 10, 10, 0, 0, 0, 0},
-                             {0, 0, 0, 0, 24, 0, 0, 0},
-                             {0, 5, 0, 15, 0, 0, 7, 0},
-                             {0, 0, 0, 0, 0, 0, 15, 0},
-                             {0, 0, 0, 0, 0, 1, 0, 9},
-                             {0, 0, 0, 0, 0, 0, 6, 13},
-                             {0, 0, 0, 0, 0, 0, 0, 16},
-                             {0, 0, 0, 0, 0, 0, 0, 0}};
+    // V = number of vertices
+    // s = source vertex
+    // t = sink vertexc
+    int V, s, t;
+    cout << "\nEnter the number of vertices in the flow network: ";
+    cin >> V;
 
-    cout << "The maximum possible flow is "
-         << fordFulkerson(C, 0, 7, V);
+    cout << "Enter the source vertex: ";
+    cin >> s;
+
+    cout << "Enter the sink vertex: ";
+    cin >> t;
+
+    // C = matrix with capacities of each edge of the flow network
+    vector<vector<int>> C(V, vector<int>(V));
+    cout << "Enter the matrix of the flow network:\n";
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            cin >> C[i][j];
+
+    cout << "\n\nThe maximum flow for the flow network is " << fordFulkerson(C, s, t, V) << "\n\n";
 
     return 0;
 }
+
+// 0 13 10 10  0  0  0  0
+// 0  0  0  0 24  0  0  0
+// 0  5  0 15  0  0  7  0
+// 0  0  0  0  0  0 15  0
+// 0  0  0  0  0  1  0  9
+// 0  0  0  0  0  0  6 13
+// 0  0  0  0  0  0  0 16
+// 0  0  0  0  0  0  0  0
+
+// 0 10  0 10  0  0
+// 0  0  4  2  8  0
+// 0  0  0  0  0 10
+// 0  0  0  0  9  0
+// 0  0  6  0  0 10
+// 0  0  0  0  0  0
